8aa2a7f422fa78f1ce2a22cea4006041d189ab50
VanHellthing <vanhellthing@mail.ru>
2025-04-20 20:47:02 +0300

    1) Добавил страницу профиля 2) Скорректировал темплейт страницы Транзакции 3) Поправил пару багулин

--- finance/accounting/urls.py
+++ finance/accounting/urls.py
@@ -7,5 +7,6 @@ app_name = 'accounting'
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("accounts/profile/", views.ProfileView.as_view(), name="profile"),
     path("transactions/", views.TransactionListView.as_view(), name="transactions")
 ]
--- finance/accounting/views.py
+++ finance/accounting/views.py
@@ -7,6 +7,31 @@ from .models import Transaction
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "index.html"
 
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "profile.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Получаем текущего пользователя
+        user = self.request.user
+
+        # Получаем последние 10 транзакций текущего пользователя, отсортированные по дате
+        transactions = Transaction.objects.filter(user=user)
+        last_transactions = transactions.order_by('-date_time')[:10]
+
+        # Вычисляем общие доходы и расходы
+        total_income = round(sum(t.amount for t in transactions if t.transaction_type == 'entry'), 2)
+        total_expenses = round(sum(t.amount for t in transactions if t.transaction_type == 'write-off'), 2)
+
+
+        # Добавляем данные в контекст      
+        context['transactions'] = last_transactions       
+        context['total_income'] = total_income
+        context['total_expenses'] = total_expenses
+
+        # Возвращаем контекст 
+        return context
 
 class TransactionListView(ListView):
     model = Transaction
