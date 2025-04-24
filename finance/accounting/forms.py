a062cf97fc51bedeba322bf31f1ba87a95cc92ba
KailRiver <kailriver06@gmail.com>
2025-04-25 00:26:09 +0300

    Added INN

--- finance/accounting/forms.py
+++ finance/accounting/forms.py
@@ -1,11 +1,21 @@
 from django import forms
-from users.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from django.core.validators import RegexValidator
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+    inn = forms.CharField(
+        max_length=12,
+        required=True,
+        validators=[
+            RegexValidator(
+                regex='^[0-9]{10,12}$',
+                message='ИНН должен содержать 10 или 12 цифр'
+            )
+        ],
+        help_text='Введите ИНН (10 или 12 цифр)'
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2',)
+        fields = ('username', 'email', 'inn', 'password1', 'password2')
