bf32a6c22d1bcf1ccb8f941f60bbdb57b677f169
KailRiver <kailriver06@gmail.com>
2025-04-27 21:40:42 +0300

    Добавил дашборды

---
+++ finance/dashboard/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard_view, name='dashboard'),
+    path('transaction-dynamics/', views.transaction_dynamics, name='transaction-dynamics'),
+    path('transaction-type-stats/', views.transaction_type_stats, name='transaction-type-stats'),
+    path('income-vs-expense/', views.income_vs_expense, name='income-vs-expense'),
+    path('transaction-status-stats/', views.transaction_status_stats, name='transaction-status-stats'),
+    path('bank-stats/', views.bank_stats, name='bank-stats'),
+    path('category-stats/', views.category_stats, name='category-stats'),
+]
